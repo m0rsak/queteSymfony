848c8c3c821cf9419da442a35d2cb79077c67d61
Cyril Bellamy <cyrilbellamy1@gmail.com>
2018-06-01 13:15:00 +0200

    solution quete FOSuserbundle

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: m0rsak
- * Date: 29/05/18
- * Time: 19:02
- */
+namespace AppBundle\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phoneNumber')
+            ->add('birthDate', DateType::class, array(
+                'placeholder' => array(
+                    'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
+                )
+            ))
+            ->add('creationDate', DateType::class, array('data' => new \DateTime('now'), 'required' => false))
+            ->add('note', IntegerType::class, array(
+                'attr' => array('min' => 0, 'max' => 20, 'label' => 'Note'),
+                'required'=> false
+            ))
+            ->add('isACertifiedPilot');
+    }
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+}
